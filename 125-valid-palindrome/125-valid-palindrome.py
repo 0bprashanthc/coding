c36f1f229abc6dbcfac80bd684e9f7ba1bdc6068
Prashanth Ch <8338748+0bprashanthc@users.noreply.github.com>
2022-06-30 21:44:47 +0100

    Time: 49 ms (90.24%), Space: 14.3 MB (85.79%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -4,10 +4,10 @@ class Solution:
         size = len(s)
         i, j = 0, size-1
         while i < j:
-            if not s[i].isalnum():
+            while i < j and not s[i].isalnum():
                 i += 1
                 continue
-            if not s[j].isalnum():
+            while i < j and not s[j].isalnum():
                 j -= 1
                 continue
             if s[i] != s[j]:
