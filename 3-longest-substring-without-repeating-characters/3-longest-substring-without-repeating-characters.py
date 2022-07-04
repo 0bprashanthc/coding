990938036073e935844d562d607cc9dd1fd18b38
Prashanth Ch <8338748+0bprashanthc@users.noreply.github.com>
2022-07-04 22:01:27 +0100

    Time: 139 ms (27.75%), Space: 14.2 MB (13.33%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,5 +1,6 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        """
         size = len(s)
         left, right = 0, 0
         chars = [0] * 128
@@ -11,5 +12,18 @@ class Solution:
                 chars[ord(s[left])] -= 1
                 left += 1
             result = max(result, right-left+1)
+            right += 1
+        return result
+        """
+        size = len(s)
+        _map = dict()
+        left, right = 0, 0
+        result = 0
+        while right < size:
+            char = s[right]
+            if char in _map:
+                left = max(left, _map[char])
+            result = max(result, right-left+1)
+            _map[char] = right + 1
             right += 1
         return result
